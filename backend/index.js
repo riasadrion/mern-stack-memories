afa90a981fcfeaefea3dfd597d7d0274a78641e5
Riasad Rion <riasadrion@gmail.com>
2021-08-29 23:27:25 +0600

    db and routes added

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); 
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://riasadrion:RY6U9dcsZPwmDNM@cluster0.go5ql.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, ()=> console.log('listening on port ' +PORT)))
+.catch((error) => console.log(error.message));
+mongoose.set('useFindAndModify', false);
