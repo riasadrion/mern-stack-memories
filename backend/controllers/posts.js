04865842d94f92d0421b4171319d4ad17c65d38c
Riasad Rion <riasadrion@gmail.com>
2022-03-11 19:26:31 +0600

    jfj

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,13 +1,13 @@
-import PostMessage from "../models/postMessage.js";
+import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages);
-    } catch (error){
-        res.status(404).json({ message: error.message });
-    }
-}
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 export const createPost = (req, res) => {
-    res.send("post Creation");
-}
+  res.send('post Creatddion');
+};
